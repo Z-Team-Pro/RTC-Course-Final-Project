6031b22e9423df6a0cdaf3528bfb8b71791bdd83
islamElbadawy <islam.201211@gmail.com>
2017-05-20 22:35:26 +0200

    My APIs

---
+++ API/MasterAPI/AddPost.php
@@ -0,0 +1,66 @@
+<?php
+
+$app->post('/RTC_Blogger/AddPost',function ($req,$res){
+
+$data= $req->getParsedBody();
+$Post_Title = $data["Post_Title"];
+$Body = $data["Body"];
+$Likes = intval($data["Likes"]);
+
+
+
+
+$files=$req->getUploadedFiles();
+$newimage=$files['Photo'];
+
+
+if ($newimage->getError() === UPLOAD_ERR_OK) {
+    $uploadFileName = $newimage->getClientFilename();
+           $type=$newimage->getClientMediaType();
+            $name = uniqid('img-'.date('Ymd').'-');
+            $name.= $newimage->getClientFilename();
+            $imgs[] = array('url' => '/Photos/' . $name);
+           
+   //local server 
+   $whitelist = array('127.0.0.1','::1');
+
+if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+ $newimage->moveTo("/home/islamelbadawy/RTC-Course-Final-Project/API/public/Photos/$name");
+        }
+else{
+
+ $newimage->moveTo("/home/islamelbadawy/RTC-Course-Final-Project/API/public/Photos/$name");
+
+}
+   
+//localdev
+      $PhotoURL="http://localhost:8080/public/Photos/$name";
+   
+  //return $res->withJson($photoURL,200);
+
+
+}
+
+
+// create new object Posts
+
+try{
+    $addpost = new $this->ParseObject("Posts");
+
+    $addpost->set("Post_Title",$Post_Title);
+    $addpost->set("Body",$Body);
+    $addpost->set("Photo",$PhotoURL);
+    $addpost->set("Likes",$Likes);
+    $addpost->save();
+    $message=array("objectid"=>$addpost->getObjectId(),"message"=>"Post Added");
+    return $res->withJson($message,200);
+     }
+
+
+catch(parseException $e){
+    return $res->withJson(" Error",200);
+}
+
+});
+
+
---
+++ API/MasterAPI/LoadPosts.php
@@ -0,0 +1,58 @@
+<?php
+
+$app->get('/RTC_Blogger/Load_Posts/[{User_ID}]',function ($req,$res,$args){
+
+if(isset($args["User_ID"])){
+    $query = new $this->ParseQuery("Posts");
+    $query->equalTo("objectId",$args["User_ID"]);
+    $results = $query->find();
+    // var_dump($results);
+    $message=[];
+    for( $i=0; $i < count($results) ;$i++){
+ 
+    $d=array('objectid'=>$results[$i]->getObjectId());
+    
+    $message[$i]=$d;
+    }
+
+
+
+  return $res->withJson($message , 200);
+
+
+
+
+
+    
+}
+else{
+    $query = new $this->ParseQuery("Posts");
+    $results = $query->find();
+    // var_dump($results);
+    $message=[];
+    for( $i=0; $i < count($results) ;$i++){
+ 
+    $d=array('objectid'=>$results[$i]->getObjectId());
+    
+    $message[$i]=$d;
+    }
+
+
+
+  return $res->withJson($message , 200);
+}
+
+
+
+
+
+});
+
+
+
+
+
+
+
+?>
+
